522247569becf684a3cf6e36f9ac59f3f78f9554
Elias Entrup <elias.entrup@tib.eu>
2024-12-04 14:15:38 +0100

    ffmpeg.js: fix import and use tmpPath

--- src/main/ffmpeg.js
+++ src/main/ffmpeg.js
@@ -2,14 +2,10 @@ const fs = require('fs')
 const path = require('path')
 const WASI = require('wasi')
 
-//import ffmpegPath from '../../resources/ffmpeg.wasm?asset'
+import ffmpegPath from '../../resources/ffmpeg.wasm?asset'
 
 // ffmpeg from https://github.com/SebastiaanYN/FFmpeg-WASI
-export async function generateThumbnails(filePath) {
-  const outPath = path.join(process.cwd(), 'out___')
-  fs.rmSync(outPath, { recursive: true, force: true })
-  fs.mkdirSync(outPath, { recursive: true })
-
+export async function generateThumbnails(filePath, tmpPath) {
   const wasi = new WASI.WASI({
     version: 'preview1',
     args: [
@@ -22,13 +18,12 @@ export async function generateThumbnails(filePath) {
     ],
     env: {},
     preopens: {
-      '/sandbox': outPath,
+      '/sandbox': tmpPath,
       '/insandbox': path.dirname(filePath)
     }
     //   stdout: myStream
   })
-  const wasm = await WebAssembly.compile(fs.readFileSync('./resources/ffmpeg.wasm'))
-  console.log(wasi)
+  const wasm = await WebAssembly.compile(fs.readFileSync(ffmpegPath)) //'./resources/ffmpeg.wasm'))
   const instance = await WebAssembly.instantiate(wasm, wasi.getImportObject())
 
   wasi.start(instance)
