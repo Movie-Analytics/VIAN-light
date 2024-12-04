2268a1057e276f6202aa77abcd42531fe9bfe42d
Elias Entrup <elias.entrup@tib.eu>
2024-12-04 14:14:42 +0100

    Add index view (to load video)

---
+++ src/renderer/src/pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-main>
+    <v-container class="fill-height flex-column">
+      <v-row justify="center">
+        <v-col align-self="center">
+          <p class="text-h1">VIAN light</p>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col align-self="center">
+          <v-btn @click="openVideo">Open video</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
+</template>
+
+<script>
+import { mapStores } from 'pinia'
+
+import { useMainStore } from '@renderer/stores/main'
+
+export default {
+  computed: {
+    ...mapStores(useMainStore)
+  },
+  methods: {
+    async openVideo() {
+      const video = await this.mainStore.open_video()
+      if (video !== undefined) this.$router.push({ path: '/videoanalysis' })
+    }
+  }
+}
+</script>
