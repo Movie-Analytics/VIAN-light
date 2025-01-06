501b7306ab51d7d470365d84b17f89b85c5051b6
Elias Entrup <elias.entrup@tib.eu>
2025-01-06 11:09:43 +0100

    main/index: provide error callback for writeFile

--- src/main/index.js
+++ src/main/index.js
@@ -144,10 +144,14 @@ ipcMain.on('save-store', (channel, name, store) => {
 
   const data = JSON.stringify(store)
   if (name == 'meta') {
-    fs.writeFile(join(dataPath, 'meta.json'), data, 'utf8')
+    fs.writeFile(join(dataPath, 'meta.json'), data, (err) => {
+      if (err) console.error('Error writing:', err)
+    })
   } else {
     fs.mkdirSync(join(dataPath, store.id), { recursive: true })
-    fs.writeFile(join(dataPath, store.id, name + '.json'), data, 'utf8')
+    fs.writeFile(join(dataPath, store.id, name + '.json'), data, (err) => {
+      if (err) console.error('Error writing:', err)
+    })
   }
 })
 
