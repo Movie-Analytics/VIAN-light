50ddc36cb7e7a537ef1a40cb3cd23f4fa2ace1cb
Elias Entrup <elias.entrup@tib.eu>
2026-02-23 14:48:20 +0100

    TimelineCanvas: prevent double click popup on locked segments

--- src/renderer/src/components/TimelineCanvas.vue
+++ src/renderer/src/components/TimelineCanvas.vue
@@ -172,7 +172,7 @@ export default {
         const [entry] = entries
         if (entry.type === 'select') {
           this.undoableStore.addVocabAnnotation(entry.id, entry.tag)
-        } else if (Date.now() - this.lastClick < 500) {
+        } else if (Date.now() - this.lastClick < 500 && !entry.locked) {
           this.doubleClickPopup(entry)
           this.tempStore.selectedSegments = new Map([[entry.id, entry.timeline]])
         } else if (
